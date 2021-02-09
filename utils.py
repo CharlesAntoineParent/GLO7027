f80c7ea0f4ce079da1ae89c9971e3ee78c4f4f6a
OlivierBelhumeur <66437332+OlivierBelhumeur@users.noreply.github.com>
2021-02-08 22:11:35 -0500

    modification des fonctions

--- utils.py
+++ utils.py
@@ -5,6 +5,12 @@ import operator
 import pandas as pd
 import dateutil.parser
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(style="ticks", color_codes=True)
+
+tips = sns.load_dataset("tips")
+
 
 configParser = configparser.RawConfigParser()
 configFilePath = r'config.txt'
@@ -20,13 +26,19 @@ def getArticle(p_hash):
         for file in selectedJsonFiles:
             with open(file, 'r') as article:
                 ArticleInfo.append(json.load(article))
+
     except UnicodeDecodeError:
-        ArticleInfo = None       
+        return None
+    except NameError:
+        return None
+    except FileNotFoundError:
+        return None
+
     return ArticleInfo
 
 
 
-def getDayInfo(p_date,p_journal) :
+def getDayInfo(p_date,p_journal):
 
     journalPath = f'{dataPath}/analytics/{p_journal}/'
 
@@ -125,7 +137,7 @@ def get_slug_from_org(p_hash):
     for org in range(len(article[0])):
         organization = article[0][org]["organizationKey"]
         slug = article[0][org]["publications"][0]["slug"]["fr"]
-        slug_dict[organization] = slug.split("/")[:-1]
+        slug_dict[organization] = slug.split("/")
     return slug_dict
 
 
@@ -145,6 +157,26 @@ def get_popularity_and_slug(p_dict_of_scores):
             slugs_and_score[hash] = None
     return slugs_and_score
 
+def slug_and_score_by_article_by_lesoleil(p_hash, p_dict_with_score_info):
+    dict_score_slug = {}
+    article_info = getArticle(p_hash)
+    present_in_info = False
+    try:
+        for org in range(len(article_info[0])):
+            if article_info[0][org]["organizationKey"] == "lesoleil":
+                present_in_info = True
+                slug = article_info[0][org]["publications"][0]["slug"]["fr"]
+                slug = slug.split("/")[0]
+                #slug = "_".join(slug)
+                dict_score_slug["slug"] = slug
+                break
+    except TypeError:
+        return None        
+    if p_dict_with_score_info[p_hash].loc["lesoleil"]["is_present"] == 0 or present_in_info != True: 
+        return None
+    dict_score_slug["score"] =  p_dict_with_score_info[p_hash].loc["lesoleil"]["score"]
+    return dict_score_slug
+
 
 
 if __name__ == "__main__":
@@ -154,28 +186,62 @@ if __name__ == "__main__":
     dict_info_total = get_all_scores_and_view_per_organization_for_a_day(p_date)
 
 
+    testing = slug_and_score_by_article_by_lesoleil("27b803e15021b1313fb1d9750a9d65a7", p_dict_with_score_info = dict_info_total)
 
 
-    hash_artile_test = "d9b71cfa7d9dfcbae96e390180fd5335"
+    #pd.DataFrame(dict_of_score_and_views_for_the_day[article_hash], columns=col_names, index=row_names)
+
+    hash_article_test = "d9b71cfa7d9dfcbae96e390180fd5335"
+    dict_slug_score_leSoleil = {"slug" : [], "score" :  [] }
+    for hash in list(dict_info_total.keys())[0:1000]:
+        dict_value = slug_and_score_by_article_by_lesoleil(hash, p_dict_with_score_info = dict_info_total)
+        if dict_value is not None:
+            dict_slug_score_leSoleil["slug"].append(dict_value["slug"])
+            dict_slug_score_leSoleil["score"].append(dict_value["score"])
+
+
+
+    test_dataframe = pd.DataFrame.from_dict(dict_slug_score_leSoleil)
+    test_dataframe = test_dataframe.groupby(['slug']).sum().reset_index()
+
+    sns.catplot(x="slug", y="score", data=test_dataframe)
+    #test_2 = slug_and_score_by_article_by_lesoleil(hash_article_test)
+
+
+
+
     testing_hashes = ['0deb8dafb131e6ac8775b2d973c2c306', '4abda728af6e702ffdccecc714ed2fcf', '531fca26523a603539db1ebd7cb5203e', 'eb096788e215d7dd54e001d2cd4423ec', '32245de2f0c9ecc1cf701f7d9ab3485c', 'ec68cd02afeb06180152a15d8cfbf9a7', '465c91f8bc131f4ff55de9c9e7a5fbed', '45dbda2e61b75a1f526a968c546c8595', 'ae340e547032d3cefa0a4fabd23f4cbc', 'af6ba32b67c53e28988beb55cf80fab0']
     for hash in testing_hashes:
         test = getArticle(hash)
         for org in range(len(test[0])):
             print("hash: ", hash)
             print("Organisation: ", test[0][org]["organizationKey"])
-            print("slug: ", test[0][org]["publications"][0]["slug"]["fr"])
+            slug = test[0][org]["publications"][0]["slug"]["fr"]
+            slug = slug.split("/")
+            slug = slug[0]
+            slug = "_".join(slug)
+            print("slug: ", slug)
             print("url: ", test[1]["url"])
             print("Scores: ")
             print(dict_info_total[hash])
             print("------------------------------------")
 
 
+
+
+
+
     #lenouvelliste = dayPopularity(p_date, "lenouvelliste")
     #lenouvelliste["odeb8dafb131e6ac8775b2d973c2c306"]
 
 
-    test_slug_fun = get_slug_from_org("4abda728af6e702ffdccecc714ed2fcf")
+    #test_slug_fun = get_slug_from_org("eb096788e215d7dd54e001d2cd4423ec")
 
-    test_slug_with_score = get_popularity_and_slug(dict_info_total)
+    #test_slug_with_score = get_popularity_and_slug(dict_info_total)
 
-    dict_info_total["702a665363bc14d4e4259ccdd98e5afd"]
+    #test = getArticle("eb096788e215d7dd54e001d2cd4423ec")
+    #dict_info_total["eb096788e215d7dd54e001d2cd4423ec"]
+
+    #test_slug_with_score["531fca26523a603539db1ebd7cb5203e"]
+
+    #len(test_slug_with_score)
